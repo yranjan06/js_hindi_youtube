24b03a2b29fc9e2664f88bb3262542833445bc86
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-02 01:07:49 +0000

    nums and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number (100)
+// console.log(balance);
+
+console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const othernumber = 123.1245
+// console.log(othernumber.toPrecision(3));
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+// *************************** Maths ***********************************
+
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.3));
+console.log(Math.floor(4.9));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
