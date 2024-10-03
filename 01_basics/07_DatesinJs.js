631a41d8db952e73f6dde55f97554c056ccfad6a
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-03 22:58:30 +0000

    Dates in JS

---
+++ 01_basics/07_DatesinJs.js
@@ -0,0 +1,20 @@
+// Date
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleString())
+// console.log(typeof myDate)
+
+
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString())
+
+
+
+
+let myTimeStamp = Date.now()
+
+console.log(myTimeStamp);
