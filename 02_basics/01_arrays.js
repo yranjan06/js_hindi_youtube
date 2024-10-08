2db693016891105c2e813e598a5a90a88be96dc2
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-08 03:25:22 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,31 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+
+const myArr2 = new Array(1, 2, 3, 4, 5)
+console.log(myArr[1]);
+
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const mewArr = myArr.join()
+
+// console.log(myArr);
+// console.log(mewArr);
+
+
+
+
