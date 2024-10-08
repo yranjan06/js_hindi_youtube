8e274fd46b971193130b38ad553d9265c86f42a1
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-08 23:25:56 +0000

    slice splice

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,14 +5,20 @@ const myHeros = ["shaktiman", "naagraj"]
 
 
 const myArr2 = new Array(1, 2, 3, 4, 5)
-console.log(myArr[1]);
+// console.log(myArr);
 
 
 // Array Methods
 
+
 // myArr.push(6)
 // myArr.push(7)
 // myArr.pop()
+
+
+
+
+
 
 // myArr.unshift(9)
 // myArr.shift()
@@ -28,4 +34,17 @@ console.log(myArr[1]);
 
 
 
+// slice, splice 
 
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+
+console.log(myn2);
