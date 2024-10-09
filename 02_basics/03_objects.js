514e31e3fc8b0f8ad8d610794afab7ded58434b8
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-09 23:47:46 +0000

    object litertal end here

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -23,9 +23,20 @@ console.log(JsUser[mySym])
 
 
 JsUser.email =  "ranjan@chatgpt.com"
-Object.freeze(JsUser)
+// Object.freeze(JsUser)
 
 JsUser.email = "ranjan@freeze.com"
-
-
 console.log(JsUser)
+
+
+
+JsUser.greeting = function(){
+    console.log("Hello Js User")
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js User,${this.name}`)
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
