3f139a2ac52936c8e1b108be373e3c025320fd9b
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-10 00:18:31 +0000

    object of object

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,24 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+ 
+tinderUser.id = "123abc"
+tinderUser.name = "Samamy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "sum@gmail.com",
+    fullname: {
+        userfullname: {
+            firstName: "Ranjan",
+            lastName: "Yadav"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstName)
+
+
+const
