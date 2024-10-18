bff8aa65c471a7b94c8c83217a328e353958ff45
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-18 02:29:38 +0000

    done with functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -15,23 +15,50 @@ console.log("N");
 // }
 
 
-function addTwonumbers(num1, num2){
-  let result = num1 + num2
-  return result
+// function addTwonumbers(num1, num2){
+//   let result = num1 + num2
+//   return result
     
+// }
+
+// const result = addTwonumbers(2, 5)
+// // console.log("Result: ", result);
+
+// function loginUserMessage(username){
+//     if(username === undefined){                            //  usrname is taken udefined which mens false in js , it can be taken as (!username)= which alsp menas false
+//         console.log("Please enter a username");  
+//         return      
+//     }
+
+//     return `${username} just logged in`
+// }
+
+// console.log(loginUserMessage())
+
+
+
+function calculateCarPrice(val1, val2, ...num1){
+  return num1
 }
 
-const result = addTwonumbers(2, 5)
-// console.log("Result: ", result);
+// console.log(calculateCarPrice(200, 400, 500, 2000))
 
-function loginUserMessage(username){
-    if(username === undefined){                            //  usrname is taken udefined which mens false in js , it can be taken as (!username)= which alsp menas false
-        console.log("Please enter a username");  
-        return      
-    }
-
-    return `${username} just logged in`
+const user = {
+  userName : "Ranjan",
+  price : 199
 }
 
-console.log(loginUserMessage())
+function handleObject(anyObject){
+  console.log(`userName is ${anyObject.userName} and price is ${anyObject.price}`);
+  
+}
 
+handleObject(user)
+
+const myArray =[200, 300, 400, 500]
+
+function returnSecondValue(getArray){
+  return getArray[1]
+}
+
+console.log(returnSecondValue(myArray))
