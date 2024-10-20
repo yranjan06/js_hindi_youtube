bc066d9301eef329c3d3bdf9a5412114bed590cd
Ranjan  Yadav <124186491+yranjan06@users.noreply.github.com>
2024-10-20 01:13:18 +0000

    scope end here

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,66 @@
+// let a = 10
+// const b = 20
+ var a = 300
+
+
+
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+    // console.log("Inner :", a);
+    
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(a);
+ 
+function one(){
+    const userName = "Ranjan"
+    
+    function two(){
+        const website = "Youtube"
+        console.log(userName);
+        
+    }
+    // console.log(website);
+
+    // two()
+    
+}
+
+// one()
+
+if (true){
+    const userName = "Ranjan02" 
+    if (userName === "Ranjan02"){
+        const website = "youtube"
+        //console.log(userName + website)
+    }
+    // console.log(website);
+    
+}
+// console.log(userName);
+
+
+
+// ******************** intresting **************************
+
+
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addone(5))
+
+
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
+
